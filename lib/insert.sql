c3364fe47b3781cfc307c74816873365f27edfe8
ned <nedschultz@outlook.com>
2023-05-02 19:41:52 +1000

    finished

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 2, 'M', 'black', 'playful', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 8, 'F', 'blonde', 'aloof', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 6, 'M', 'brown', 'strict', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 3, 'F', 'brown', 'sweet', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 10, 'M', 'green', 'grumpy', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'white', 'friendly', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, NULL, 'F', NULL, NULL, true);
